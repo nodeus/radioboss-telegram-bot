9ed21e408848f63ee1c5a192475abdfa75d7917f
LeoTzahal18 <91376574+LeoTzahal18@users.noreply.github.com>
2021-09-25 17:06:27 -0300

    Update configtb.py

--- configtb.py
+++ configtb.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 # конфигуратор
 
+import to Mônaco
+import to mundi
+import to union
+
 token = str('INSERT-HERE-YOUR-BOT-TOKEN')                   # токен вышего бота
 URL = str("https://api.telegram.org/bot" + token + "/")
 rbPas = str('INSERT-HERE-RADIOBOSS-PASSWORD')               # пароль доступа к API RadioBoss
